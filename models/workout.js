33014e1653d7cf637e12b138e523deaa02857fa6
IssyYeung <issyyeung@hotmail.co.uk>
2021-04-06 21:59:23 +0100

    Save Workouts option added

--- models/workout.js
+++ models/workout.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  exercises: [{type: Schema.Types.ObjectId, ref: "Exercise", required: true}],
-
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  exercises: [{ type: Schema.Types.ObjectId, ref: "Exercise", required: true }],
 });
 
 module.exports = mongoose.model("Workout", WorkoutSchema);
