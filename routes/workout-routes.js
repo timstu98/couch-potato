ad4f1d642dab1f65658732545fe32ab24fe9ed9f
bella <tianqibu@hotmail.com>
2021-04-06 19:19:33 +0100

    Add preferences to user if user wants to save

--- routes/workout-routes.js
+++ routes/workout-routes.js
@@ -1,46 +1,45 @@
 const ExerciseModel = require("../models/exercise");
+const UserModel = require("../models/user")
 const func = require("../functions")
 
 module.exports = function (app) {
-    // simple get request to test if working:
-    app.get("/random", async function (_, res) {
-      let e = await ExerciseModel.find({});
-      let output = e[Math.floor(Math.random() * e.length) + 1];
-      try {
-        res.json(output);
-      } catch (error) {
-        res.status(500).send(error);
-      }
-    });
-  
-    // to generate a workout:
-    // (assuming I already have user preferences)
+   
     app.get("/workout", async function (req, res) {
-      // inputs from the user:
+     
       let time = req.query.time;
       let musclegroup = req.query.musclegroup;
       let difficulty = req.query.difficulty;
-      let type = req.query.type; // strength vs tone
+      let type = req.query.type;
+      let id = req.query.id;
+      let save = req.query.save;
   
-      // test output in postman:
-      // let output = {
-      //   time: time,
-      //   muscleGroup: muscleGroup,
-      //   level: level,
-      //   type: type,
-      // };
-  
+      // could be better to decode the JWT token to get the ID 
+      // but i'm not sure how to get that from the post request header
+
+      if (save === "yes" && id) {
+      let user = await UserModel.findByIdAndUpdate(
+            {_id: id}, 
+            {preferences: { time: time, 
+                            musclegroup: musclegroup, 
+                            difficulty: difficulty, 
+                            type: type}}, 
+            function(err) {
+              if (err) {
+                res.send(err);
+              }});
+      }
+          
       // calculating how many exercises to fetch:
       let numOfEx;
       if (type === "strength") {
         numOfEx = time / 5; // time taken per exercise (5 sets, 5 reps, 45sec breaks, based on 3 sec per rep)
       } else if (type === "tone") {
         numOfEx = time / 3.5; // time taken per exercise (3 sets, 12 reps, 30sec breaks, based on 3 sec per rep)
+      } else {
         console.log(
-          "Type of workout not valid. Please enter 'strength' or 'tone'."
-        );
+        "Type of workout not valid. Please enter 'strength' or 'tone'.");
       }
-  
+    
       // fetch appropriate exercises from the db
       let exercises = await ExerciseModel.find({
         musclegroup: musclegroup[0].toUpperCase() + musclegroup.substring(1), // have to make first letter lower case
@@ -64,3 +63,5 @@ module.exports = function (app) {
     });
 
 }
+
+
