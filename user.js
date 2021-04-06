7c92927f0b9e3562850c4876b65b6699055d5075
Shay-commits <78738471+Shay-commits@users.noreply.github.com>
2021-04-06 16:11:04 +0100

    modified schemas
    Users now have preferences and generated workouts are linked to individual users.

---
+++ user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {type:String,required:true},
+  password: {type:String,required:true,minlength:8},
+  email: String,
+  admin: Boolean,
+  prefences: {
+    time: {type: Number},
+    musclegroup: {
+      type: String,
+      enum: ["Upperbody", "Lowerbody", "Core"],
+      default: "Upperbody",
+    },
+    difficulty: { type: String, enum: ["Beginner", "Intermediate", "Advanced"]},
+    type: { type: String, enum:["strength", "tone"] }
+
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
