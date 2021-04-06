82a152c5434ea3048c3753d9f110e7316e863633
sixianchen17 <sixian.chen17@gmail.com>
2021-04-06 10:32:02 +0100

    Added schema validation

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: String,
-  password: String,
+  username: {type:String,required:true},
+  password: {type:String,required:true,minlength:8},
   email: String,
   admin: Boolean
 });
