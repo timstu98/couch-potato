32bf96c517ff366b17789304070eb38123ce23ca
Lionel Smith <smithlionel1121@gmail.com>
2021-04-20 09:38:55 +0100

    Base structure

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const port = 3000;
+const port = 5000;
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
