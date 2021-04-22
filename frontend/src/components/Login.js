eba54b4741e056ba0a7722f853fd21d044e06254
dmoore <darren.m95@icloud.com>
2021-04-22 13:57:09 +0100

    Add Navbar and Footer components to Login component and add fieldset and legend html elements to Login component form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,6 +3,8 @@ import { useForm } from 'react-hook-form';
 import { login } from '../context/app-actions';
 import AppContext from '../context/app-context';
 import { Redirect } from 'react-router';
+import Navbar from './navbar/Navbar';
+import Footer from './footer/Footer';
 
 const Login = () => {
   const { dispatch, isAuthenticated } = useContext(AppContext);
@@ -23,49 +25,54 @@ const Login = () => {
     <Redirect to='/' />
   ) : (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor='username'>Username</label>
-          <input
-            id='username'
-            placeholder='Username'
-            {...register('username', {
-              required: 'Please enter a username',
-              minLength: {
-                value: 6,
-                message: 'Minimum 6 characters are allowed',
-              },
-              maxLength: {
-                value: 255,
-                message: 'Maximum 255 characters are allowed',
-              },
-            })}
-          />
-          {errors.username && <p style={{ color: 'red' }}>{errors.username.message}</p>}
-        </div>
+      <Navbar />
+      <fieldset>
+        <legend>Login</legend>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor='username'>Username</label>
+            <input
+              id='username'
+              placeholder='Username'
+              {...register('username', {
+                required: 'Please enter a username',
+                minLength: {
+                  value: 6,
+                  message: 'Minimum 6 characters are allowed',
+                },
+                maxLength: {
+                  value: 255,
+                  message: 'Maximum 255 characters are allowed',
+                },
+              })}
+            />
+            {errors.username && <p style={{ color: 'red' }}>{errors.username.message}</p>}
+          </div>
 
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            placeholder='Password'
-            {...register('password', {
-              required: 'Please enter a password',
-              minLength: {
-                value: 6,
-                message: 'Minimum 6 characters are allowed',
-              },
-              maxLength: {
-                value: 255,
-                message: 'Maximum 255 characters are allowed',
-              },
-            })}
-          />
-          {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
-        </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              placeholder='Password'
+              {...register('password', {
+                required: 'Please enter a password',
+                minLength: {
+                  value: 6,
+                  message: 'Minimum 6 characters are allowed',
+                },
+                maxLength: {
+                  value: 255,
+                  message: 'Maximum 255 characters are allowed',
+                },
+              })}
+            />
+            {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
+          </div>
 
-        <input type='submit' value='Login' />
-      </form>
+          <input type='submit' value='Login' />
+        </form>
+      </fieldset>
+      <Footer />
     </div>
   );
 };
