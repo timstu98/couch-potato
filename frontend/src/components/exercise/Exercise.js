cb74300014d298a869f0db5c4a1bbc14331ec992
dmoore <darren.m95@icloud.com>
2021-04-23 00:44:18 +0100

    Add Exercises and Exercise components

--- frontend/src/components/exercise/Exercise.js
+++ frontend/src/components/exercise/Exercise.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import { FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import './Exercise.css';
+import { FaTimes, FaCheck } from 'react-icons/fa';
 
-const Exercise = ({ exercise }) => {
-  return <div></div>;
+const Exercise = ({ exercise, onComplete, onDelete }) => {
+  return (
+    <div className={`exercise ${exercise.completed ? 'completed completed-text' : ''}`}>
+      <h3>
+        {exercise.name}
+        <div className='icons'>
+          <FaCheck style={{ color: '#4682b4', cursor: 'pointer' }} onClick={() => onComplete(exercise._id)} />
+          <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(exercise._id)} />
+        </div>
+      </h3>
+      <h4>{exercise.musclegroup}</h4>
+      <h5>{exercise.difficulty}</h5>
+    </div>
+  );
+};
+
+Exercise.propTypes = {
+  exercise: PropTypes.object,
+  onComplete: PropTypes.func,
+  onDelete: PropTypes.func,
 };
 
 export default Exercise;
--- frontend/src/components/exercises/Exercises.js
+++ frontend/src/components/exercises/Exercises.js
@@ -1,119 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Exercise from '../exercise/Exercise';
+import PropTypes from 'prop-types';
 
-const [exercises, setExercises] = useState([
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0bfc',
-    name: 'Halo',
-    link: 'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/394/halo/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c06',
-    name: 'Power Push Down',
-    link: 'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/286/power-push-down/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0bf5',
-    name: 'Inchworms',
-    link: 'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/254/inchworms/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0bfe',
-    name: 'Medicine Ball Push-ups',
-    link: 'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/40/medicine-ball-push-ups/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c0d',
-    name: 'Spinal Twist with a Push-Pull Movement',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/219/spinal-twist-with-a-push-pull-movement/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c13',
-    name: 'High Plank T-spine Rotation',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/330/high-plank-t-spine-rotation/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c0d',
-    name: 'Spinal Twist with a Push-Pull Movement',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/219/spinal-twist-with-a-push-pull-movement/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c00',
-    name: 'TRX Suspended Knee Tucks',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/87/trx-reg-suspended-knee-tucks/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c01',
-    name: 'TRX Side-straddle Golf Swings',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/81/trx-reg-side-straddle-golf-swings/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c13',
-    name: 'High Plank T-spine Rotation',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/330/high-plank-t-spine-rotation/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0c03',
-    name: 'Offset Single-arm Chest Press',
-    link:
-      'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/356/offset-single-arm-chest-press/',
-    __v: 0,
-  },
-  {
-    musclegroup: 'Upperbody',
-    difficulty: 'Advanced',
-    _id: '607f3ca688c1673a446d0bf5',
-    name: 'Inchworms',
-    link: 'https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/254/inchworms/',
-    __v: 0,
-  },
-]);
+const Exercises = ({ data }) => {
+  const updData = data.map((exercise) => {
+    return { ...exercise, completed: false };
+  });
 
-const Exercises = () => {
+  const [exercises, setExercises] = useState(updData);
+
+  const deleteExercise = (id) => {
+    setExercises(exercises.filter((exercise) => exercise._id !== id));
+  };
+
+  const completeExercise = (id) => {
+    setExercises(
+      exercises.map((exercise) => (exercise._id === id ? { ...exercise, completed: !exercise.completed } : exercise)),
+    );
+  };
+
   return (
     <div>
-      {exercises.map((exercise, _id) => (
-        <Exercise key={_id} exercise={exercise} onDelete={onDelete} />
+      {exercises.map((exercise) => (
+        <Exercise key={exercise._id} exercise={exercise} onComplete={completeExercise} onDelete={deleteExercise} />
       ))}
     </div>
   );
+};
+
+Exercises.propTypes = {
+  data: PropTypes.array,
 };
 
 export default Exercises;
